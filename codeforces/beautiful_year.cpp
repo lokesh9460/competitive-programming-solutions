ba27e0857b13340653d4c1b746e17e84f4675b87
lokesh9460 <39955241+lokesh9460@users.noreply.github.com>
2021-02-17 23:27:50 +0530

    commit

--- codeforces/beautiful_year.cpp
+++ codeforces/beautiful_year.cpp
@@ -59,15 +59,15 @@ typedef map<char, int> mci;
 
 void solve()
 {
-    int n;
+    int n, a, b, c, d;
     cin >> n;
     while (true)
     {
-        n += 1;
-        int a = n / 1000;
-        int b = n / 100 % 10;
-        int c = n / 10 % 10;
-        int d = n % 010;
+        n ++;
+        a = n / 1000;
+        b = (n / 100) % 10;
+        c = (n / 10) % 10;
+        d = n % 10;
         if (a != b && a != c && a != d && b != c && b != d && c != d)
             break;
     }
